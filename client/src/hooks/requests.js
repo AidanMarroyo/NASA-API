ee355217676e0a5fb136b9ebe7f21ca9b9531a66
aidanMarroyo <aidanarroyostudent@gmail.com>
2024-04-04 20:42:48 -0400

    initial commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,50 @@
+const API_URL = 'http://localhost:8080/v1';
+
+async function httpGetPlanets() {
+  // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  // ascending order by flight number
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'delete',
+    });
+  } catch (error) {
+    return {
+      ok: false,
+    };
+  }
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
